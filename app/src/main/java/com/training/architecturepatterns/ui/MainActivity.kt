c2c35066227ebee9fc2dea86cfdfd49297ac145f
Farouk Jabberi <jabberi.contact@gmail.com>
2020-09-10 22:43:31 +0200

    MVP --> MVVM

--- app/src/main/java/com/training/architecturepatterns/ui/MainActivity.kt
+++ app/src/main/java/com/training/architecturepatterns/ui/MainActivity.kt
@@ -3,24 +3,24 @@ package com.training.architecturepatterns.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.training.architecturepatterns.R
 import com.training.architecturepatterns.models.MovieModel
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), View.OnClickListener, MovieView {
-    private lateinit var presenter: MoviePresenter
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var viewModel: MovieViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener(this)
-        presenter = MoviePresenter(this)
+        viewModel = ViewModelProvider(this).get(MovieViewModel::class.java)
+        viewModel.movieMutableLiveData.observe(this, Observer { textView.text = it.toString() })
     }
 
     override fun onClick(view: View?) {
-        if (view == button) presenter.getMovieDetails()
+        if (view == button) viewModel.getMovieDetails()
     }
 
-    override fun onGetMovie(movieModel: MovieModel) {
-        textView.text = movieModel.toString()
-    }
 }
